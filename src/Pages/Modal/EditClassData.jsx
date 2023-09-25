c8cabfa37289c979be54b8f7bfeda5cc15b8d4b8
iinaamasum <iinaamasum@gmail.com>
2023-09-25 16:27:23 +0600

    Modal updated

--- src/Pages/Modal/EditClassData.jsx
+++ src/Pages/Modal/EditClassData.jsx
@@ -2,12 +2,14 @@ import {
   Dialog,
   DialogBody,
   DialogHeader,
+  Input,
   Option,
   Select,
 } from '@material-tailwind/react';
 import { useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useForm } from 'react-hook-form';
+import { MdCancel } from 'react-icons/md';
 import { useNavigate } from 'react-router-dom';
 import auth from '../../firebase.init';
 
@@ -21,7 +23,11 @@ const EditClassData = ({ open, handleOpen }) => {
   const [user] = useAuthState(auth);
   const [newClassView, setNewClassView] = useState(false);
 
-  const [selectSlot, setSelectSlot] = useState('Slot 1');
+  const [selectPeriod, setSelectPeriod] = useState('');
+  const [selectCategory, setSelectCategory] = useState('Theory');
+  const [selectRoom, setSelectRoom] = useState('');
+  const [selectRoomError, setSelectRoomError] = useState(false);
+  const [selectPeriodError, setSelectPeriodError] = useState(false);
 
   const allClass = {
     1695575501670: {
@@ -48,6 +54,26 @@ const EditClassData = ({ open, handleOpen }) => {
     },
   };
 
+  const handleSelectedCourse = (c) => {
+    if (selectRoom === '') {
+      setSelectRoomError(true);
+    }
+    if (selectPeriod === '' && selectCategory === 'Theory') {
+      setSelectPeriodError(true);
+      console.log(selectPeriodError);
+      return;
+    }
+    if (selectRoom === '') {
+      return;
+    }
+    const addedData = {
+      ...c,
+      selectRoom,
+      selectCategory,
+    };
+    console.log(addedData);
+  };
+
   return (
     <>
       <Dialog
@@ -61,30 +87,77 @@ const EditClassData = ({ open, handleOpen }) => {
           onClick={handleOpen}
           className="h-[30px] w-[30px] min-h-16 flex items-center justify-center absolute top-2 right-2 bg-gray-300 rounded-full cursor-pointer"
         >
-          <span className="text-red-600 font-extrabold text-xl">x</span>
+          <span className="text-red-600 font-extrabold text-xl">
+            <MdCancel />
+          </span>
         </p>
 
         <DialogHeader className="w-full flex items-center justify-center text-3xl mt-5">
           Add Class
         </DialogHeader>
         <DialogBody className="pb-8 border-0">
-          <div className="w-[97%] md:w-[90%] mx-auto px-4 py-5">
+          <div className="w-[97%] md:w-[90%] mx-auto px-4 pb-2">
             <Select
-              value={selectSlot}
+              value={selectCategory}
               onChange={(e) => {
-                setSelectSlot(e);
+                setSelectCategory(e);
               }}
               size="lg"
-              label="Select Slot"
+              label="Course Category"
             >
-              <Option value="Slot 1">Slot 1</Option>
-              <Option value="Slot 2">Slot 2</Option>
-              <Option value="Slot 3">Slot 3</Option>
+              <Option value="Lab">Lab</Option>
+              <Option value="Theory">Theory</Option>
             </Select>
+          </div>
+
+          {selectCategory === 'Theory' && (
+            <div className="w-[97%] md:w-[90%] mx-auto px-4 pt-2 pb-2">
+              <Select
+                value={selectPeriod}
+                onChange={(e) => {
+                  setSelectPeriod(e);
+                  setSelectPeriodError(false);
+                }}
+                size="lg"
+                label="Select Period"
+              >
+                <Option value="Period1">Period 1</Option>
+                <Option value="Period2">Period 2</Option>
+                <Option value="Period3">Period 3</Option>
+              </Select>
+
+              {selectPeriodError && (
+                <p className="text-red-500 font-semibold text-[12px] pt-1">
+                  ❌ Period is not added.
+                </p>
+              )}
+            </div>
+          )}
+
+          <div className="w-[97%] md:w-[90%] mx-auto px-4 pt-2 pb-5">
+            <Input
+              onChange={(e) => {
+                setSelectRoom(e.target.value);
+                setSelectRoomError(false);
+              }}
+              size="lg"
+              label="Room Number"
+              type="text"
+              name="room_number"
+              min="0"
+            />
+            {selectRoomError && (
+              <p className="text-red-500 font-semibold text-[12px] pt-1">
+                ❌ Room Number is not provided.
+              </p>
+            )}
           </div>
           <div className="grid grid-cols-3 gap-3 text-center w-[97%] md:w-[90%] mx-auto px-4 text-gray-800 -mb-3">
             {Object.values(allClass).map((c) => (
-              <div className="border-[2px] py-2 px-1 rounded-md border-blue-gray-400 hover:cursor-pointer hover:bg-blue-gray-200 hover:text-black transition-all duration-100">
+              <div
+                onClick={() => handleSelectedCourse(c)}
+                className="border-[2px] py-2 px-1 rounded-md border-blue-gray-400 hover:cursor-pointer hover:bg-blue-gray-200 hover:text-black transition-all duration-100"
+              >
                 <p>{c.course_short_form}</p>
                 <p className="text-[13px]">Credit: {c.credit}</p>
                 <p>
